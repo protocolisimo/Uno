a9124f4ce8c6821bb8aba0e57ec5800b310c37b4
protocolisimo <protocolisimo@gmail.com>
2025-01-07 19:48:02 +0100

    feat: wip

--- app/app/game/game.tsx
+++ app/app/game/game.tsx
@@ -1,57 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { View, Button, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import io from 'socket.io-client';
 
-const socket = io('http://localhost:3500');
+function Game({ gameState, id, handleDrawCard, handlePlayCard, onLeave }) {
 
-const Game = () => {
-  const [gameState, setGameState] = useState({
-    players: [],
-    deck: [],
-    discardedPile: [],
-    currentPlayer: 0,
-    gameDirection: 1,
-  });
-  const [id, setId] = useState('')
-
-  useEffect(() => {
-    socket.on('gameState', (state) => {
-      setGameState(state);
-    });
-
-    socket.on('connect_error', (err) => {
-      console.log('Connection error:', err.message);
-    });
-
-    return () => {
-      socket.off('gameState');
-    };
-  }, []);
-
-  useEffect(() => {
-    if (socket !== null) {
-      socket.emit('joinGame', socket.id)
-      setId(socket.id)
-    }
-  }, [socket])
+  // useEffect(() => {
+  //   return () => onLeave
+  // }, [])
 
   const playCard = (cardIndex: number) => {
-
-    socket.emit('playCard', {
-      playerId: gameState.players[gameState.currentPlayer].id,
-      cardIndex,
-    });
+    handlePlayCard(cardIndex);
   };
 
   const drawCard = () => {
-    socket.emit('drawCard', gameState.players[gameState.currentPlayer].id);
+    handleDrawCard();
   };
-
-  console.log({socket})
 
   return (
     <View style={styles.container}>
-      <Text>{id}setDeck{socket?.id      }</Text>
+      <Text>{id}</Text>
       <View style={{ position: 'relative', width: '100%', height: 100 }}>
         {gameState.discardedPile.map((card, i) => (
           <View
