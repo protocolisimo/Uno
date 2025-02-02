af8cda97b10d0633a3c5b7622bd06ff58a0aec0e
protocolisimo <protocolisimo@gmail.com>
2025-02-02 12:29:15 +0100

    WIP

--- app/app/game/game.tsx
+++ app/app/game/game.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { View, Button, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { BLACK, OTHER_COLORS, WHITE } from '@/constants/Colors';
+import { typography } from '@/constants/Typography';
+import React from 'react';
+import { View, Button, Text, TouchableOpacity, StyleSheet, SafeAreaView, Image } from 'react-native';
+import deckImage from '../../assets/images/deck.png';
 
-function Game({ gameState, id, handleDrawCard, handlePlayCard, onLeave }) {
-
-  // useEffect(() => {
-  //   return () => onLeave
-  // }, [])
+function Game({ gameState: { discardedPile, players, deck, currentPlayer }, id, handleDrawCard, handlePlayCard }) {
 
   const playCard = (cardIndex: number) => {
     handlePlayCard(cardIndex);
@@ -15,60 +14,125 @@ function Game({ gameState, id, handleDrawCard, handlePlayCard, onLeave }) {
     handleDrawCard();
   };
 
+  const thePlayer = players.find((player) => player.id === id)
+
+  console.log(thePlayer, players[0].hand, id);
+  
+
   return (
-    <View style={styles.container}>
-      <Text>{id}</Text>
-      <View style={{ position: 'relative', width: '100%', height: 100 }}>
-        {gameState.discardedPile.map((card, i) => (
-          <View
-            key={i}
-            style={{
-              position: 'absolute',
-              backgroundColor: card.color === 'wild' ? 'black' : card.color,
-              padding: 10,
-              margin: 5,
-            }}
-          >
-            <Text style={{ color: 'black', fontSize: 30 }}>{card.type}</Text>
-          </View>
-        ))}
-      </View>
+    <SafeAreaView>
+      <View style={styles.topBarWrapper}>
+        <View style={styles.playersWrapper}>
+          {players.map((player) => (
+            <View >
+              <View style={styles.avatarWrapper}>
 
-      {gameState.players.map((player, index) => (
-        <View key={player.id} style={{ margin: 10 }}>
-          <Text>{`Player ${index + 1}`}</Text>
-          <View style={{ flexDirection: 'row' }}>
-            {player.hand.map((card, i) => (
-              <TouchableOpacity
-                key={i}
-                onPress={() => {
-                  if (index === gameState.currentPlayer) playCard(i);
-                }}
-                style={{
-                  backgroundColor: card.color === 'wild' ? 'black' : card.color,
-                  padding: 10,
-                  margin: 5,
-                }}
-              >
-                <Text style={{ color: 'black', fontSize: 30 }}>{card.type}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
+                <Text style={styles.avatar}>
+                  {player.id[0]}{player.id[1]}
+                </Text>
+              </View>
+              <View style={styles.handDisplayWrapper}>
+                <Text>
+                  {player.hand.length.toString()}
+                </Text>
+              </View>
+            </View>
+          ))}
         </View>
-      ))}
+        <View>
+          <Image source={deckImage} style={styles.deckImage} />
+          <Text>
+            {deck.length.toString()}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <Text>
+          {players[currentPlayer]?.id === id ? 'Your turn' : players[currentPlayer]?.id}
+        </Text>
+      </View>
+      <View>
+        {discardedPile.map(((card) => (
+          <Text>
+            {card.color}
+            {card.type.toString()}
+          </Text>
+        )))}
+      </View>
+      <View>
+        {thePlayer?.hand.map(((card, index) => (
+          <TouchableOpacity onPress={() => playCard(index)}>
+            <Text>
+              {card.color}
+              {card.type.toString()}
+            </Text>
+          </TouchableOpacity>
+        )))}
 
-      <Button title="Draw Card" onPress={drawCard} />
-
-    </View>
+      </View>
+      <View>
+        {/* // controls */}
+        <View>
+          {/* // uno button */}
+        </View>
+        <View>
+          {/* // you */}
+        </View>
+        <View>
+          {/* // dont have it button */}
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Game;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  topBarWrapper: {
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16
   },
+  playersWrapper: {
+    flexDirection: "row",
+    gap: 12,
+  },
+  avatarWrapper: {
+    width: 38,
+    height: 38,
+    borderRadius: '50%',
+    borderColor: OTHER_COLORS.brightGray,
+    borderWidth: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: BLACK,
+    marginBottom: 11,
+  },
+  avatar: {
+    fontWeight: 'bold',
+    color: WHITE
+  },
+  handDisplayWrapper: {
+    position: "absolute",
+    bottom: 2,
+    height: 17,
+    minWidth: 36,
+    backgroundColor: WHITE,
+    borderRadius: 36,
+    borderWidth: 1,
+    borderColor: OTHER_COLORS.brightGray,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  handDisplay: {
+    ...typography.HELVETICA.Caption2,
+    color: BLACK,
+    fontWeight: "bold"
+  },
+  deckImage: {
+    width: 47,
+    height: 64
+  }
 });
---
+++ app/constants/Typography.ts
@@ -0,0 +1,33 @@
+export const typography = {
+    HELVETICA: {
+        H: {
+            fonFamily: 'Helvetica',
+            fontSize: 50,
+            fontWeight: 'bold'
+        },
+        Heading: {
+            fonFamily: 'Helvetica',
+            fontSize: 26
+        },
+        SubHeading: {
+            fonFamily: 'Helvetica',
+            fontSize: 20
+        },
+        Text1: {
+            fonFamily: 'Helvetica',
+            fontSize: 18
+        },
+        Text2: {
+            fonFamily: 'Helvetica',
+            fontSize: 16
+        },
+        Caption1: {
+            fonFamily: 'Helvetica',
+            fontSize: 14
+        },
+        Caption2: {
+            fonFamily: 'Helvetica',
+            fontSize: 12
+        }
+    }
+}
